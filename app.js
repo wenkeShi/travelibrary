939654cc54820f9b84832e4ebeb03f03482dd66c
Shiwenke <swenke@foxmail.com>
2017-10-22 23:46:12 +0800

    后台login接口调试

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if(res.code){
+          wx.request({
+            url: 'https://liudongtushuguan.cn/login',
+            data: {
+              code: res.code,
+            },
+            success:function(res){
+              if(res.statusCode==200){
+                console.log(res.data);
+              }
+            },
+          });
+        }else{
+          console.log(res.errMsg);
+        }
       }
     })
     // 获取用户信息
@@ -20,8 +35,8 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
+              this.globalData.userInfo = res.userInfo;
+              console.log(res.userInfo);
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
