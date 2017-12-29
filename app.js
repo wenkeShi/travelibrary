1fa62b348426f7e5a4db71db44f57d5ec8a8aa06
shiwenke <shiwenke@fiberhome.com>
2017-12-29 14:48:58 +0800

    修改应用启动时动作

--- app.js
+++ app.js
@@ -95,6 +95,25 @@ App({
               }
             }
           })
+        }else{
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success:() => {
+              // 用户已经同意小程序授权，后续调用wx.getUserInfo 接口不会弹窗询问
+              wx.getUserInfo({
+                success: res => {
+                  // 可以将 res 发送给后台解码出 unionId
+                  this.globalData.userInfo = res.userInfo;
+                  console.log(res.userInfo);
+                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                  // 所以此处加入 callback 以防止这种情况
+                  if (this.userInfoReadyCallback) {
+                    this.userInfoReadyCallback(res)
+                  }
+                }
+              })
+            },
+          })
         }
       }
     })
