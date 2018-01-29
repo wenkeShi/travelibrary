6af53c6726ba08b7784684fa64804d0fa093bdb3
Shiwenke <swenke@foxmail.com>
2018-01-29 22:32:07 +0800

    delete annotation

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力   
-    // var logs = wx.getStorageSync('logs') || [];
     // var sessionId = wx.getStorageSync('sessionId');
     // console.log(sessionId);
     // logs.unshift(Date.now());
