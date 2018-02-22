8850050e2e08c0fa507e3069997fdbd14fa581c3
Shiwenke <swenke@foxmail.com>
2018-02-22 22:27:08 +0800

    delete annotation

--- pages/booktravel/index.js
+++ pages/booktravel/index.js
@@ -55,7 +55,6 @@ Page({
     wx.onSocketClose(function(res){
       console.log('websocket closed');
     });
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
     //   // 所以此处加入 callback 以防止这种情况
     //   app.userInfoReadyCallback = res => {
     //     this.setData({
