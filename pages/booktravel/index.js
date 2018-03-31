a20c642e9f4e2a7d176dd471bef16318374c2e22
Shiwenke <swenke@foxmail.com>
2018-03-31 13:18:53 +0800

    delete annotation

--- pages/booktravel/index.js
+++ pages/booktravel/index.js
@@ -55,7 +55,7 @@ Page({
     wx.onSocketClose(function(res){
       console.log('websocket closed');
     });
-    //   // 在没有 open-type=g
+    //   // 在没有 open-type=
     function login(){
       wx.request({
         url: URL + '/login',
